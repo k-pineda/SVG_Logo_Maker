123300bcdca790e7b04fc99a192f13a45d908cfd
k-pineda <karenp9310@gmail.com>
2023-09-13 02:46:02 -0400

    modified shapelist.test to match with shapelist.js change

--- tests/shapeList.test.js
+++ tests/shapeList.test.js
@@ -1,4 +1,4 @@
-const ShapeList = require('../lib/shapeList ');
+const ShapeList = require('../lib/shapeList');
 
 test('ShapeList.render() should render SVG shape based on input', () => {
   const circle = ShapeList.render('circle');
@@ -6,6 +6,6 @@ test('ShapeList.render() should render SVG shape based on input', () => {
   const square = ShapeList.render('square');
 
   expect(circle).toMatch('<circle cx="100" cy="100" r="50" />');
-  expect(triangle).toMatch('<polygon points="100,25 75,100 125,100" />');
+  expect(triangle).toMatch('<polygon points="100,25 60,100 145,100" />');
   expect(square).toMatch('<rect x="50" y="50" width="100" height="100" />');
 });
